3381e6cb31643dd6b9704253c87eb2393a79f406
Tareeq Hamilton <tareeqhamilton@gmail.com>
2020-11-17 17:23:26 -0500

    added local storage get functionality

--- index.js
+++ index.js
@@ -8,6 +8,15 @@ $(document).ready(function(){
     localStorage.setItem(hour, userInput);
 
     })
+
+    $("#9am .description").val(localStorage.getItem("9am"));
+    $("#10am .description").val(localStorage.getItem("10am"));
+    $("#11am .description").val(localStorage.getItem("11am"));
+    $("#12pm .description").val(localStorage.getItem("12pm"));
+    $("#1pm .description").val(localStorage.getItem("1pm"));
+    $("#2pm .description").val(localStorage.getItem("2pm"));
+    $("#3pm .description").val(localStorage.getItem("3pm"));
+    $("#4pm .description").val(localStorage.getItem("4pm"));
     // create event for which button fired 
     // retrieve the input field value
     // save to the local storage for hour name and for value
